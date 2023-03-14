22cf8c58f0314154d6fe8663496208c26c6ff517
LightningV08 <m5.lavrentyev@yandex.ru>
2023-03-14 23:36:33 +0300

    Cleaned AES.java

--- app/src/main/java/lightningv08/cryptonite/AES.java
+++ app/src/main/java/lightningv08/cryptonite/AES.java
@@ -2,16 +2,12 @@ package lightningv08.cryptonite;
 
 import android.content.Context;
 import android.net.Uri;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.google.common.io.ByteStreams;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
@@ -24,8 +20,6 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.util.Arrays;
 import java.util.Base64;
-import java.util.Random;
-import java.util.stream.Collectors;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -57,7 +51,7 @@ public class AES {
     public AES(String key) {
         try {
             this.key = getKeyFromPassword(key, SALT);
-            this.iv = generateIVBytes();
+            this.iv = generateIV();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -81,22 +75,7 @@ public class AES {
         return keyGen.generateKey();
     }
 
-    public static String generateIV() {
-        int leftLimit = 97; // letter 'a'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 16;
-        Random random = new Random();
-        StringBuilder buffer = new StringBuilder(targetStringLength);
-        for (int i = 0; i < targetStringLength; i++) {
-            int randomLimitedInt = leftLimit + (int)
-                    (random.nextFloat() * (rightLimit - leftLimit + 1));
-            buffer.append((char) randomLimitedInt);
-        }
-
-        return buffer.toString();
-    }
-
-    public static byte[] generateIVBytes() throws NoSuchAlgorithmException {
+    public static byte[] generateIV() throws NoSuchAlgorithmException {
         byte[] b = new byte[16];
         SecureRandom.getInstanceStrong().nextBytes(b);
         return b;
@@ -138,67 +117,8 @@ public class AES {
                 .array();
     }
 
-    private String readInputStream(InputStream inputStream) throws IOException {
-        ByteArrayOutputStream result = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        for (int length; (length = inputStream.read(buffer)) != -1; ) {
-            result.write(buffer, 0, length);
-        }
-        return result.toString("UTF-8");
-    }
-
     private byte[] readInputStreamBytes(InputStream inputStream) throws IOException {
         return ByteStreams.toByteArray(inputStream);
-    }
-
-    private String getIvFromFile(String s) {
-        //return s.substring(0, s.offsetByCodePoints(0, 16));
-        return s.substring(0, 16);
-    }
-
-    private String getContentFromFile(String s) {
-        return s.substring(16);
-    }
-
-    public static String encrypt(String key, String iv, String msg) throws Exception {
-        byte[] bytesOfKey = key.getBytes(StandardCharsets.UTF_8);
-        MessageDigest md = MessageDigest.getInstance("SHA256");
-        byte[] keyHash = md.digest(bytesOfKey);
-
-        final byte[] ivBytes = iv.getBytes();
-
-        SecretKeySpec secretKeySpec = new SecretKeySpec(keyHash, "AES");
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, new IvParameterSpec(ivBytes));
-
-        final byte[] resultBytes = cipher.doFinal(msg.getBytes());
-        return Base64.getMimeEncoder().encodeToString(resultBytes);
-    }
-
-    public static String encrypt(byte[] key, String iv, String msg) throws Exception {
-        final byte[] ivBytes = iv.getBytes();
-
-        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, new IvParameterSpec(ivBytes));
-
-        final byte[] resultBytes = cipher.doFinal(msg.getBytes());
-        return Base64.getMimeEncoder().encodeToString(resultBytes);
-    }
-
-    public static String encrypt(SecretKey key, String iv, String msg)
-            throws NoSuchPaddingException, NoSuchAlgorithmException,
-            InvalidAlgorithmParameterException, InvalidKeyException,
-            IllegalBlockSizeException, BadPaddingException {
-        final byte[] ivBytes = iv.getBytes();
-
-        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getEncoded(), "AES");
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, new IvParameterSpec(ivBytes));
-
-        final byte[] resultBytes = cipher.doFinal(msg.getBytes());
-        return Base64.getEncoder().encodeToString(resultBytes);
-        //return new String(resultBytes, StandardCharsets.UTF_8);
     }
 
     public static byte[] encrypt(SecretKey key, byte[] iv, byte[] msg)
@@ -223,52 +143,5 @@ public class AES {
         cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
 
         return cipher.doFinal(encrypted);
-    }
-
-    public static String decrypt(SecretKey key, String iv, String encrypted)
-            throws NoSuchPaddingException, NoSuchAlgorithmException,
-            InvalidAlgorithmParameterException, InvalidKeyException,
-            IllegalBlockSizeException, BadPaddingException {
-        final byte[] ivBytes = iv.getBytes();
-
-        final byte[] encryptedBytes = Base64.getDecoder().decode(encrypted);
-        //final byte[] encryptedBytes = encrypted.getBytes(StandardCharsets.UTF_8);
-
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(ivBytes));
-
-        final byte[] resultBytes = cipher.doFinal(encryptedBytes);
-        //return new String(resultBytes, StandardCharsets.UTF_8);
-        return Base64.getEncoder().encodeToString(resultBytes);
-    }
-
-    public static String decrypt(String key, String iv, String encrypted) throws Exception {
-        byte[] bytesOfKey = key.getBytes(StandardCharsets.UTF_8);
-        MessageDigest md = MessageDigest.getInstance("SHA256");
-        byte[] keyHash = md.digest(bytesOfKey);
-
-        final byte[] ivBytes = iv.getBytes();
-
-        final byte[] encryptedBytes = Base64.getMimeDecoder().decode(encrypted);
-
-        SecretKeySpec secretKeySpec = new SecretKeySpec(keyHash, "AES");
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new IvParameterSpec(ivBytes));
-
-        final byte[] resultBytes = cipher.doFinal(encryptedBytes);
-        return new String(resultBytes);
-    }
-
-    public static String decrypt(byte[] key, String iv, String encrypted) throws Exception {
-        final byte[] ivBytes = iv.getBytes();
-
-        final byte[] encryptedBytes = Base64.getMimeDecoder().decode(encrypted);
-
-        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new IvParameterSpec(ivBytes));
-
-        final byte[] resultBytes = cipher.doFinal(encryptedBytes);
-        return new String(resultBytes);
     }
 }
