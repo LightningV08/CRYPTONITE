bc010993d4a8f81d8be6bd277181c961e3e6c6df
LightningV08 <m5.lavrentyev@yandex.ru>
2023-04-12 22:39:49 +0300

    3DES encryption support

---
+++ app/src/main/java/lightningv08/cryptonite/TripleDES.java
@@ -0,0 +1,72 @@
+package lightningv08.cryptonite;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class TripleDES extends FileEncrypter {
+
+    private final SecretKey key;
+    private final byte[] iv;
+    private static final String SALT = "!CRYPTONITE_3DES";
+
+    public TripleDES(String key) {
+        try {
+            this.key = getKeyFromPassword(key, SALT, 192);
+            this.iv = generateIV();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public byte[] encrypt(SecretKey key, byte[] iv, byte[] msg)
+            throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException, NoSuchProviderException {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getEncoded(), "DES");
+        Cipher cipher = Cipher.getInstance("DESede/CBC/NoPadding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, new IvParameterSpec(iv));
+
+        return cipher.doFinal(msg);
+    }
+
+    @Override
+    public byte[] decrypt(SecretKey key, byte[] iv, byte[] encrypted)
+            throws IllegalBlockSizeException, BadPaddingException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getEncoded(), "DES");
+        Cipher cipher = Cipher.getInstance("DESede/CBC/NoPadding");
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new IvParameterSpec(iv));
+
+        return cipher.doFinal(encrypted);
+    }
+
+    @Override
+    protected SecretKey getKey() {
+        return key;
+    }
+
+    @Override
+    protected byte[] getIv() {
+        return iv;
+    }
+
+    @Override
+    protected int getIvSize() {
+        return 8;
+    }
+}
