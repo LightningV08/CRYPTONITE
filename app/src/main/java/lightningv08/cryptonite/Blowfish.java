64890b75d7a3646ddb3f231f7542b53f110abc0a
LightningV08 <m5.lavrentyev@yandex.ru>
2023-04-12 23:12:30 +0300

    Blowfish encryption support

---
+++ app/src/main/java/lightningv08/cryptonite/Blowfish.java
@@ -0,0 +1,62 @@
+package lightningv08.cryptonite;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Blowfish extends FileEncrypter {
+
+    private final SecretKey key;
+    private final byte[] iv;
+    private static final String SALT = "!CRYPTONITE_BLOW";
+
+    public Blowfish(String key) {
+        try {
+            this.key = getKeyFromPassword(key, SALT, 256);
+            this.iv = generateIV();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public byte[] encrypt(SecretKey key, byte[] iv, byte[] msg) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getEncoded(), "Blowfish");
+        Cipher cipher = Cipher.getInstance("Blowfish");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, new IvParameterSpec(iv));
+
+        return cipher.doFinal(msg);
+    }
+
+    @Override
+    public byte[] decrypt(SecretKey key, byte[] iv, byte[] encrypted) throws IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getEncoded(), "Blowfish");
+        Cipher cipher = Cipher.getInstance("Blowfish");
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new IvParameterSpec(iv));
+
+        return cipher.doFinal(encrypted);
+    }
+
+    @Override
+    protected SecretKey getKey() {
+        return key;
+    }
+
+    @Override
+    protected byte[] getIv() {
+        return iv;
+    }
+
+    @Override
+    protected int getIvSize() {
+        return 16;
+    }
+}
---
+++ app/src/main/java/lightningv08/cryptonite/BlowfishEncryptActivity.java
@@ -0,0 +1,64 @@
+package lightningv08.cryptonite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import lightningv08.cryptonite.databinding.ActivityEncryptBinding;
+
+public class BlowfishEncryptActivity extends AppCompatActivity {
+
+    private ActivityEncryptBinding binding;
+    private final int FILE_SELECT_CODE = 1;
+    private Uri uri;
+    private String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityEncryptBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.chooseFileButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("*/*");
+            startActivityForResult(intent, FILE_SELECT_CODE);
+        });
+        binding.encryptButton.setOnClickListener(v -> {
+            if (uri == null) {
+                Toast.makeText(this, "Choose file", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            password = binding.password.getText().toString();
+            if (!password.isEmpty() && !password.equals(binding.confirmPassword.getText().toString())) {
+                Toast.makeText(this, "Passwords don't match", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Blowfish blowfish = new Blowfish(password);
+            try {
+                blowfish.encryptFileIv(getApplicationContext(), uri);
+                Toast.makeText(this, "File encrypted", Toast.LENGTH_SHORT).show();
+                setResult(RESULT_OK, getIntent());
+            } catch (Exception e) {
+                Log.e("LightningV08", e.getMessage());
+                Toast.makeText(this, "Encryption error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case FILE_SELECT_CODE:
+                    uri = data.getData();
+                    break;
+            }
+        }
+    }
+}
