3ffd5c6077095b25966d4d13f09dbd468ba801a7
LightningV08 <m5.lavrentyev@yandex.ru>
2023-04-13 11:55:08 +0300

    GOST28147 encryption support

---
+++ app/src/main/java/lightningv08/cryptonite/GOST28147.java
@@ -0,0 +1,64 @@
+package lightningv08.cryptonite;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class GOST28147 extends  FileEncrypter{
+
+    private final SecretKey key;
+    private final byte[] iv;
+    private static final String SALT = "!CRYPTONITE_GOST";
+
+        public GOST28147(String key) {
+        try {
+            this.key = getKeyFromPassword(key, SALT, 256);
+            this.iv = generateIV();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public byte[] encrypt(SecretKey key, byte[] iv, byte[] msg) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchProviderException {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getEncoded(), "GOST28147");
+        Cipher cipher = Cipher.getInstance("GOST28147/CBC/PKCS5Padding", "SC");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, new IvParameterSpec(iv));
+
+        return cipher.doFinal(msg);
+    }
+
+    @Override
+    public byte[] decrypt(SecretKey key, byte[] iv, byte[] encrypted) throws IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getEncoded(), "GOST28147");
+        Cipher cipher = Cipher.getInstance("GOST28147/CBC/PKCS5Padding", "SC");
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new IvParameterSpec(iv));
+
+        return cipher.doFinal(encrypted);
+    }
+
+    @Override
+    protected SecretKey getKey() {
+        return key;
+    }
+
+    @Override
+    protected byte[] getIv() {
+        return iv;
+    }
+
+    @Override
+    protected int getIvSize() {
+        return 8;
+    }
+}
---
+++ app/src/main/java/lightningv08/cryptonite/GOSTDecryptActivity.java
@@ -0,0 +1,64 @@
+package lightningv08.cryptonite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import lightningv08.cryptonite.databinding.ActivityDecryptBinding;
+
+public class GOSTDecryptActivity extends AppCompatActivity {
+
+    private ActivityDecryptBinding binding;
+    private final int FILE_SELECT_CODE = 1;
+    private Uri uri;
+    private String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDecryptBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.chooseFileButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("*/*");
+            startActivityForResult(intent, FILE_SELECT_CODE);
+        });
+        binding.decryptButton.setOnClickListener(v -> {
+            if (uri == null) {
+                Toast.makeText(this, "Choose file", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            password = binding.password.getText().toString();
+            if (!password.isEmpty() && !password.equals(binding.confirmPassword.getText().toString())) {
+                Toast.makeText(this, "Passwords don't match", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            GOST28147 gost28147 = new GOST28147(password);
+            try {
+                gost28147.decryptFileIv(getApplicationContext(), uri);
+                Toast.makeText(this, "File decrypted", Toast.LENGTH_SHORT).show();
+                setResult(RESULT_OK, getIntent());
+            } catch (Exception e) {
+                Log.e("LightningV08", e.toString());
+                Toast.makeText(this, "Decryption error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case FILE_SELECT_CODE:
+                    uri = data.getData();
+                    break;
+            }
+        }
+    }
+}
